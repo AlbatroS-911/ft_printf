295bdab87d597bd101af5f8bb6aa2de442583df0
Toky Rabemanantsoa <tokyerick4@gmail.com>
2026-02-27 06:50:14 +0300

    Update ft_print_add.c

--- ft_print_add.c
+++ ft_print_add.c
@@ -12,17 +12,30 @@
 
 #include "ft_printf.h"
 
+static int	ft_puthex_add(unsigned long num)
+{
+	int		counter;
+	char	*base;
+
+	counter = 0;
+	base = "0123456789abcdef";
+	if (num >= 16)
+		counter += ft_puthex_add(num / 16);
+	ft_putchar_fd(base[num % 16], 1);
+	return (counter + 1);
+}
+
 int	ft_print_add(void *address, int fd)
 {
 	int	counter;
 
+	counter = 0;
 	if (address == NULL)
 	{
-		counter = ft_putstr_fd("(nil)", fd);
+		counter += ft_putstr_fd("(nil)", fd);
 		return (counter);
-	}	
-	counter = 0;
+	}
 	ft_putstr_fd("0x", 1);
-	counter = ft_putnbr_base((unsigned long)address, "0123456789abcdef");
+	counter += ft_puthex_add((unsigned long)address);
 	return (ft_strlen("0x") + counter);
 }
